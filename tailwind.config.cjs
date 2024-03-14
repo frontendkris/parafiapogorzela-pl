7e633a0b3f53f10ef7c3ad59c3ef990f68b37fec
Krzysztof Król <krzysztof.krol@royalcode.eu>
2024-03-14 02:03:01 +0100

    Fix HD image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
     container: {
-      padding: '1rem',
-      '@screen xl': {
-        padding: '2rem',
+      padding: "1rem",
+      "@screen xl": {
+        padding: "2rem",
       },
     },
-		extend: {
+    extend: {
       colors: {
-        'red': '#FF0029',
-        'luckypoint': '#293459',
-        'bastille': '#2B2B35',
-        'nero': '#1E1E1E',
+        red: "#FF0029",
+        luckypoint: "#293459",
+        bastille: "#2B2B35",
+        nero: "#1E1E1E",
       },
       screens: {
-        "xs": "375px",
-        '3xl': '1800px',
-      }
+        xs: "375px",
+        "3xl": "1800px",
+      },
     },
-	},
-	plugins: [
-    require('@tailwindcss/typography'),
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
     function ({ addComponents }) {
       addComponents({
-        '.container': {
+        ".container": {
           "@screen 3xl": {
-            maxWidth: '1800px',
+            maxWidth: "1800px",
           },
-        }
-      })
-    }
+        },
+      });
+    },
   ],
-}
+};
